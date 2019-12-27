6fbb58398390a46282ef9b0c2433144516245775
Jon Mellman <jonmellman@gmail.com>
2019-12-26 23:28:35 -0500

    Refactor music logic into music.ts

--- src/game.ts
+++ src/game.ts
@@ -1,60 +1,6 @@
+import { Interval, Key, Quality } from "./music"
+
 // Nothing Alexa-specific in this file.
-
-// https://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
-export enum Note {
-	C4 = 60,
-	D4 = 62,
-	E4 = 64,
-	F4 = 65,
-	G4 = 67,
-	A4 = 69,
-	B4 = 71,
-	C5 = 72
-}
-
-export enum Key {
-	C = 0,
-	'C#',
-	D,
-	Eb,
-	E,
-	F,
-	'F#',
-	G,
-	Ab,
-	A,
-	Bb,
-	B,
-}
-
-export enum Interval {
-	ROOT = 0,
-	MINOR_SECOND,
-	MAJOR_SECOND,
-	MINOR_THIRD,
-	MAJOR_THIRD,
-	PERFECT_FOURTH,
-	TRITONE,
-	PERFECT_FIFTH,
-	MINOR_SIXTH,
-	MAJOR_SIXTH,
-	MINOR_SEVENTH,
-	MAJOR_SEVENTH,
-	OCTAVE
-}
-
-const Quality = {
-	MAJOR: [
-		Interval.ROOT,
-		Interval.MAJOR_SECOND,
-		Interval.MAJOR_THIRD,
-		Interval.PERFECT_FOURTH,
-		Interval.PERFECT_FIFTH,
-		Interval.MAJOR_SIXTH,
-		Interval.MAJOR_SEVENTH,
-		Interval.OCTAVE
-	]
-}
 
 interface Level {
 	description: string,
@@ -107,16 +53,6 @@ export const levels: Level[] = [{
 	targetIntervals: Quality.MAJOR,
 	numRounds: 5
 }]
-
-// Given a game level, select two pitches
-// Given a pitch, return the audio file name
-// Given two pitches, calculate the distance
-// Given the distance, return the correct answer(s)
-
-
-// Level 1: (C4), (D4, E4, F4)
-// Level 2: (C4), (G4, A4, B4)
-// Level 3: (C4), (C4, D4, E4, F4, G4, A4, B4)
 
 export const getNewGame = (level: GameSession['level'] = 1): LevelInProgress => {
 	return {
---
+++ src/music.ts
@@ -0,0 +1,55 @@
+// https://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
+export enum Note {
+	C4 = 60,
+	D4 = 62,
+	E4 = 64,
+	F4 = 65,
+	G4 = 67,
+	A4 = 69,
+	B4 = 71,
+	C5 = 72
+}
+
+export enum Key {
+	C = 0,
+	'C#',
+	D,
+	Eb,
+	E,
+	F,
+	'F#',
+	G,
+	Ab,
+	A,
+	Bb,
+	B,
+}
+
+export enum Interval {
+	ROOT = 0,
+	MINOR_SECOND,
+	MAJOR_SECOND,
+	MINOR_THIRD,
+	MAJOR_THIRD,
+	PERFECT_FOURTH,
+	TRITONE,
+	PERFECT_FIFTH,
+	MINOR_SIXTH,
+	MAJOR_SIXTH,
+	MINOR_SEVENTH,
+	MAJOR_SEVENTH,
+	OCTAVE
+}
+
+export const Quality = {
+	MAJOR: [
+		Interval.ROOT,
+		Interval.MAJOR_SECOND,
+		Interval.MAJOR_THIRD,
+		Interval.PERFECT_FOURTH,
+		Interval.PERFECT_FIFTH,
+		Interval.MAJOR_SIXTH,
+		Interval.MAJOR_SEVENTH,
+		Interval.OCTAVE
+	]
+}
